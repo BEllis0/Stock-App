38be3c1eec32d9ce84fcbb7b494e3a549e4ba481
Brandon Ellis <adk_skier@yahoo.com>
2019-12-08 14:56:35 -0700

    db connection

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
+// set the connection options, which will be applied to all connections
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri)
+    .then(() => console.log("MongoDB database connection established successfully"))
+    .catch(err => console.log(err));
 
 
 app.listen(port, () => {
