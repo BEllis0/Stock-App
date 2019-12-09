237d63876fd8a5c37a8c73be75460bf3263245d6
Brandon Ellis <adk_skier@yahoo.com>
2019-12-08 21:08:34 -0700

    includes password hashing

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // for future authentication
-// const bcrypt = require('bcryptjs');
-// const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+const SALT_WORK_FACTOR = 10;
 
 const { isEmail } = require('validator');
 
@@ -35,33 +35,33 @@ const UserSchema = new Schema({
 
 //hashing password; phase 2 
 
-// UserSchema.pre(save, function(next) {
-//     var user = this;
+UserSchema.pre('save', function(next) {
+    var user = this;
 
-//     // only hash the password if it has been modified (or is new)
-//     if (!user.isModified('password')) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-//     // generate a salt
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-//         if (err) return next(err);
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
 
-//         // hash the password using the new salt
-//         bcrypt.hash(user.password, salt, function(err, hash) {
-//             if (err) return next(err);
+        // hash the password using the new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
 
-//             // override the cleartext password with the hashed one
-//             user.password = hash;
-//             next();
-//         });
-//     });
-// });
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
 
-// UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//         if (err) return cb(err);
-//         cb(null, isMatch);
-//     });
-// };
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 const User = mongoose.model('User', UserSchema);
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+// --- For getting data
+
 router.get('/', (req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
+
+//get user info by their ID
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+// --- For posting data
 
 // handles post requests to add new user to the database
 router.post('/newuser', (req, res) => {
@@ -21,7 +32,7 @@ router.post('/newuser', (req, res) => {
 
     // .save is a mongoose method to save to db
     newUser.save()
-        .then(() => res.json(`User Added:`))
+        .then(() => res.json(`User Added: ${email} ${username} ${password}`))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
