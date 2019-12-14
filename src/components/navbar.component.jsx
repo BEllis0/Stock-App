acf73206dfb137b78a02397aa831d0cc98e760fc
Brandon Ellis <adk_skier@yahoo.com>
2019-12-13 18:39:34 -0700

    style updates

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -8,9 +8,43 @@ export default class Navbar extends React.Component {
     constructor() {
         super();
         this.state = {
-
+            newsItems: [
+                // structure 
+            ],
+            stockName: 'stocks',
+            email: '',
+            username: '',
+            password: '',
         };
+
+        this.onChangeStock = this.onChangeStock.bind(this);
+
     };
+
+    // fetches news api data on page load, taking 'stock' as initial enpoint
+    // when user searches for a stock, new endpoint is used
+    componentDidMount() {
+        fetch(`http://localhost:5000/top-news/${this.state.stockName}`)
+        .then(res => res.json())
+        .then(articles => {
+
+            console.log(articles);
+
+            this.setState({
+                newsItems: [articles],
+            }, console.log(this.state.newsItems));
+        })
+        .catch(err => console.log(err));
+
+        console.log(this.state.newsItems);
+    };
+
+    // handles user typing in stock name
+    onChangeStock(event) {
+        this.setState({
+            stockName: event.target.value,
+        }, console.log(this.state.stockName))
+    }
 
     render() {
     
@@ -28,7 +62,7 @@ export default class Navbar extends React.Component {
                 </div>
                 </Toolbar>
                 <form className="stock-input-form">
-                <TextField id="standard-search" label="Search for stocks" type="search" />
+                <TextField id="standard-search" label="Search for stocks" type="search" onChange={this.onChangeStock} />
                 <Button className="search-button" type="submit">Search</Button>
                 </form>
             </AppBar>
