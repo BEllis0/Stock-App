f29dfe022cd1dd2d1d69cf9fa940721cf8cfd0dc
Brandon Ellis <adk_skier@yahoo.com>
2019-12-14 12:46:40 -0700

    added public url

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import App from './App';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<Router><Route component={App}></Route></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><Route basename={process.env.PUBLIC_URL + '/'} component={App}></Route></Router>, document.getElementById('root'));
