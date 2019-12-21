259f2fb92e0140732467b59ec7474dce854b815b
Brandon Ellis <adk_skier@yahoo.com>
2019-12-21 09:23:53 -0500

    finalized design view

--- src/components/news-view.component.jsx
+++ src/components/news-view.component.jsx
@@ -2,42 +2,47 @@ import React from 'react';
 
 export default function NewsView(props) {
 
-    if (props.newsItems) {
+    {/* If api is pulling in news */}
+    if (props.newsItems.length >= 1) {
 
     return (
 
-        <div>
+    <div className="newsLayout">
             <h1>Top Stories</h1>
-            {/* {console.log(props.newsItems[0])} */}
-            {props.newsItems.map(article => {
+        
+            <ul className="newsList">
+            {props.newsItems[0].map(article => {
+                
                 return (
-                    <div className="mainNewsStory">
-                    <img src={article.image} className="newsImg" />
-                    <p>{article.source}</p>
-                    <h1>{article.title}</h1>
-                    <p>{article.description}</p>
+                    <div className="newsArticle" key={props.newsItems[0].indexOf(article)}>
+                    <a href={article.url} target="_blank" rel="noopener noreferrer">
+                    <img className="newsImg" src={article.urlToImage} alt={article.title} />
+                    <p className="articleSourceName">{article.source.name}</p>
+                    <h4 className="articleTitle">{article.title}</h4>
+                    </a>
+                    {/* <p className="articleDesc">{article.description}</p> */}
+                    <p className="articleDate">{article.publishedAt.slice(0,10)}</p>
                     </div>
                 )
             })}
-        </div>
-        )
+            </ul>
+
+
+            
+        {/* } */}
+    </div>
+    )
     }
 
     else {
         return (
-            <div>
-                <h1>Top Stories</h1>
-                <h2>News stream unavailable</h2>
+            <div className="newsLayout">
+            
+            <h1>Top Stories</h1>
+            <div className="newsUnavailableLayout">
+                <h3>Loading..</h3>
+            </div>
             </div>
         )
     }
 };
-
-// function NewsItem(props)  {
-
-//     return (
-//         <p>{props.description}</p>
-//     )
-// }
-
-// export default NewsItem;
