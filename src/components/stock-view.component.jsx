a08c8de351270def997822f6f7e51f483e51f03a
Brandon Ellis <adk_skier@yahoo.com>
2019-12-21 09:25:19 -0500

    added state connections

--- src/components/stock-view.component.jsx
+++ src/components/stock-view.component.jsx
@@ -3,8 +3,24 @@ import React from 'react';
 export default function StockView(props) {
     
         return (
-            <div>
-                <h1>Stockview</h1>
+            <div className="stockPageLayout">
+                <div>
+                {props.stockCurrent.map(stock => {
+                    return (
+                        <h1 key={props.stockCurrent.indexOf(stock)}>
+                        {stock['Global Quote']['01. symbol']}
+                        </h1>
+                    )
+                })}
+                <h4>{props.company}</h4>
+                {props.stockTimeSeriesMinute.map(stock => {
+                    return (
+                        <h3 key={props.stockTimeSeriesMinute.indexOf(stock)}>
+                        {stock['Time Series (1min)'][Object.keys(props.stockTimeSeriesMinute[0]['Time Series (1min)'])[0]]['4. close']}</h3>
+                    )
+                })}
+                </div>
             </div>
         );
 };
+
