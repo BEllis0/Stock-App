743c0443a2a0c1b39bd33d7b5efa1d26eb6f9c37
Brandon Ellis <adk_skier@yahoo.com>
2020-01-03 00:25:35 -0700

    creating menu component

---
+++ src/components/menu.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Toolbar, AppBar, Divider, TextField } from '@material-ui/core';
+
+const Menu = (props) => {
+
+    return (
+        <div className="MenuLayout">
+            <h1>Menu</h1>
+        </div>
+    )
+
+};
+
+export default Menu;
