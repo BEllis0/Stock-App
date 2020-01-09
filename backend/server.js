37ff07be012b6eb4c42620b916e78a8fc08e40ec
Brandon Ellis <adk_skier@yahoo.com>
2020-01-08 21:08:39 -0700

    prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 //middleware
 app.use(cors());
