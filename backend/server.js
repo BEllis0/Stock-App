877c38a8b6faaf4e9c125303c0c4b9c9b8fd6558
Brandon Ellis <adk_skier@yahoo.com>
2020-01-13 15:27:26 -0700

    prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -150,8 +150,8 @@ app.get('/earnings-calendar/:date', (req, res) => {
 })
 
 // -- FOR DEPLOYMENT TO HEROKU
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static((path.join(__dirname, 'build'))));
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
