b3825deaf93e4c2557057d66feebc3152801ff1b
Brandon Ellis <adk_skier@yahoo.com>
2020-01-13 13:24:06 -0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use('/users', usersRouter);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static((path.join(__dirname, 'build'))));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });    
 }
