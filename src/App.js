f7a988ba0bb70aedf4f0301c79b301851b8557ba
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-16 16:08:02 -0600

    changed db get stocks endpoint

--- src/App.js
+++ src/App.js
@@ -201,7 +201,7 @@ export default class App extends React.Component {
   getDbStocks() {
     // if user is logged in, get their watchlist
     if(this.state.loggedIn && this.state.userId !== 0) {
-      Axios.get(`/api/users/saved-stocks/${this.state.userId}`)
+      Axios.get(`/api/stocks/saved-stocks/${this.state.userId}`)
       .then(stock => {
         console.log(stock);
 
