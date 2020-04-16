b6282ed96da61a088512591254b4c4c78fdbc181
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-16 15:33:36 -0600

    added all control handlers

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -1,6 +1,20 @@
 let User = require('../models/user.model.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const axios = require('axios');
+require('dotenv').config();
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+
+const fetchNews = (searchTerm, pageNum) =>
+    newsapi.v2.everything({
+      q: searchTerm,
+      language: 'en',
+      sortBy: 'relevancy',
+      page: pageNum,
+      pageSize: 12,
+    });
 
 module.exports = {
     users: {
@@ -95,7 +109,87 @@ module.exports = {
                     })
                     .catch(err => res.status(400).json("Error: " + err));
             }
+        },
+        alphavantage: {
+            stocks: {
+                search: (req, res) => {
+                    axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${req.params.keywords}&apikey=${process.env.STOCK_API_KEY}`)
+                        .then(response => res.json(response.data))
+                        .catch(err => res.status(400).json("Error" + err));
+                },
+                current: (req, res) => {
+                    axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.params.stock}&apikey=${process.env.STOCK_API_KEY}`)
+                        .then(response => res.json(response.data))
+                        .catch(err => res.status(400).json("Error: " + err));
+                },
+                intraday: (req, res) => {
+                    //function options:
+                    //TIME_SERIES_INTRADAY -- multiple times per day (see interval options)
+
+                    //interval options: 1min, 5min, 15min, 30min, 60min
+
+                    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${req.params.stock}&interval=${req.params.time}&outputsize=full&apikey=${process.env.STOCK_API_KEY}`)
+                        .then(response => res.json(response.data))
+                        .catch(err => res.status(400).json("Error: " + err));
+                },
+                timeSeries: (req, res) => {
+                    //function options:
+                    //TIME_SERIES_DAILY
+                    //TIME_SERIES_WEEKLY
+                    //TIME_SERIES_MONTHLY
+
+                    if(req.params.time === 'TIME_SERIES_DAILY') {
+                        axios.get(`https://www.alphavantage.co/query?function=${req.params.time}&symbol=${req.params.stock}&outputsize=full&apikey=${process.env.STOCK_API_KEY}`)
+                            .then(response => res.json(response.data))
+                            .catch(err => res.status(400).json("Error: " + err));
+                    }
+                    else {
+                        axios.get(`https://www.alphavantage.co/query?function=${req.params.time}&symbol=${req.params.stock}&apikey=${process.env.STOCK_API_KEY}`)
+                            .then(response => res.json(response.data))
+                            .catch(err => res.status(400).json("Error: " + err));
+                    }
+                }
+            },
+            indicators: {
+                
+                //interval options: 1min, 5min, 15min, 30min, 60min, daily, weekly, monthly
+                //time-period (# of data points) options: any number; recommended 200
+                
+                rsi: (req, res) => {
+                    axios.get(`https://www.alphavantage.co/query?function=RSI&symbol=${req.params.stock}&interval=${req.params.interval}&time_period=${req.params.timeperiod}&series_type=open&apikey=${process.env.STOCK_API_KEY}`)
+                        .then(response => res.json(response.data))
+                        .catch(err => res.status(400).json("Error" + err));
+                },
+                ema: (req, res) => {
+                    axios.get(`https://www.alphavantage.co/query?function=EMA&symbol=${req.params.stock}&interval=${req.params.interval}&time_period=${req.params.time-period}&series_type=close&apikey=${process.env.STOCK_API_KEY}`)
+                        .then(response => res.json(response.data))
+                        .catch(err => res.status(400).json("Error" + err));
+                },
+                sma: (req, res) => {
+                    axios.get(`https://www.alphavantage.co/query?function=SMA&symbol=${req.params.stock}&interval=${req.params.interval}&time_period=${req.params.time-period}&series_type=close&apikey=${process.env.STOCK_API_KEY}`)
+                        .then(response => res.json(response.data))
+                        .catch(err => res.status(400).json("Error" + err));
+                }
+            }
         }
+    },
+    news: {
+        get: {
+            bySearchTerm: (req, res) => {
+                const topic = req.params.search;
+                fetchNews(topic, 1)
+                    .then(response => {
+                        res.json(response.articles);
+                    })
+                    .catch(error => console.log(error));
+            }
+        }
+    },
+    earnings_cal: (req, res) => {
+        // free trial ended, looking for new earnings cal api solution
+        axios.get(`https://api.earningscalendar.net/?date=${req.params.date}`)
+            .then(response => res.json(response.data))
+            .catch(err => res.status(400).json("Error" + err));
     },
     login: (req, res) => {
         //find user by email 
@@ -148,5 +242,5 @@ module.exports = {
     },
     //TODO LOGOUT ROUTE ====
 
-    
+
 };
