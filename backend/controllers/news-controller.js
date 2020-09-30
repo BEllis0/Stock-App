007201f2f2ee2cccc34c18484ed75dfea3b44eed
Brandon Ellis <brandonellis.email@gmail.com>
2020-09-29 20:01:55 -0600

    added handler for sentiment

--- backend/controllers/news-controller.js
+++ backend/controllers/news-controller.js
@@ -23,8 +23,19 @@ module.exports = {
                     .catch(error => console.log(error));
             },
             sentiment: (req, res) => {
-                // TODO
-                // https://finnhub.io/docs/api#news-sentiment
+                axios.get(`https://finnhub.io/api/v1/news-sentiment?symbol=${req.params.symbol}`, {
+                        headers: {
+                            'X-Finnhub-Token': process.env.FINNHUB_API_KEY
+                        }
+                    })
+                    .then(response => {
+                        console.log('News Sentiment', response.data);
+                        res.status(200).json(response.data);
+                    })
+                    .catch(err => {
+                        console.log("Error getting news sentiment", err);
+                        res.status(400).json(err);
+                    });
             }
         }
     },
