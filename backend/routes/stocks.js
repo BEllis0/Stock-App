03826b6aef26b78f8f3bf7e1ae760f7901e18bf3
Brandon Ellis <brandonellis.email@gmail.com>
2020-09-29 22:35:58 -0600

    added routes for finnhub stock and company apis

--- backend/routes/stocks.js
+++ backend/routes/stocks.js
@@ -1,11 +1,48 @@
 const router = require('express').Router();
 const stocksController = require('../controllers/stocks-controller.js');
 
+// =================
 // --- USER STOCKS
+// =================
+
 router.get('/saved-stocks/:id', stocksController.stocks.get.stockById); // get watchlist by user id
 router.post('/new-stock/:id', stocksController.stocks.post.newStock); // add new stock; currently replaces the entire watchlist with req
 
+
+// =================
+// Finnhub API
+// =================
+
+// Candle timeseries data
+router.get('/stock-timeseries/:interval:from/:to/:stock', stocksController.stocks.finnhub.stocks.timeSeries);
+
+// Stock Quote
+router.get('/quote/:symbol', stocksController.stocks.finnhub.stocks.quote);
+
+// realtime websocket
+// TBD
+
+// Stock Symbol SEARCH
+router.get('/stock-search/:symbol', stocksController.stocks.finnhub.stocks.search);
+
+// Company profile
+router.get('/company-profile/:symbol', stocksController.stocks.finnhub.company.profile);
+
+// Company Financials
+router.get('/company-profile/:symbol', stocksController.stocks.finnhub.company.financials);
+
+// Indicators
+
+//EMA TBD
+
+//SMA TBD
+
+// RSI TBD
+
+
+// =================
 // Alphavantage API
+// =================
 
 // (TIMESERIES: INTRADAY)
 router.get('/stock-timeseries-intra/:time/:stock', stocksController.stocks.alphavantage.stocks.intraday);
@@ -30,5 +67,5 @@ router.get('/stock-ema/:stock/:interval/:time-period', stocksController.stocks.a
 // STOCK SEARCH
 router.get('/stock-search/:keywords', stocksController.stocks.alphavantage.stocks.search);
 
-
+// export
 module.exports = router;
