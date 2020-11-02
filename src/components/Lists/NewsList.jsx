bed9aeda6db1eabf1f4522ae479dfc935acdd621
Brandon Ellis <brandonellis.email@gmail.com>
2020-11-02 13:56:17 -0700

    moved news list display

--- src/components/Lists/NewsList.jsx
+++ src/components/Lists/NewsList.jsx
@@ -10,15 +10,15 @@ const NewsList = (props) => {
                         
                         <a href={article.url} target="_blank" rel="noopener noreferrer">
                             <img className="newsImg" src={article.urlToImage} alt={article.title} />
-                            <p className="articleSourceName">{article.source.name}</p>
-                            <h4 className="articleTitle">{article.title}</h4>
+                            <p className="articleSourceName" style={{color: props.colorDisplay === 'dark' ? 'white' : ''}}>{article.source.name}</p>
+                            <h4 className="articleTitle" style={{color: props.colorDisplay === 'dark' ? 'white' : ''}}>{article.title}</h4>
                         </a>
 
                         <p className="articleDate">{article.publishedAt.slice(0,10)}</p>
                     </div>
                 )
             })}
-            </ul>
+        </ul>
     )
 };
 
