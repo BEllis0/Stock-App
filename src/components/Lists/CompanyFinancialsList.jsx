b1781a4830f3a03f40a93543fee3c9aaaa3ca2b0
Brandon Ellis <brandonellis.email@gmail.com>
2020-11-02 13:55:59 -0700

    formatted financial data, added styling

--- src/components/Lists/CompanyFinancialsList.jsx
+++ src/components/Lists/CompanyFinancialsList.jsx
@@ -6,30 +6,26 @@ export default function CompanyFinancialsList(props) {
     let { companyFinancials } = props;
 
     const columns = [
-        { field: 'id', headerName: 'Metric', width: 250 },
-        // { field: 'metric', headerName: 'Metric', width: 250 },
-        { field: 'value', headerName: 'Value', width: 400 }
+        { field: 'id', headerName: 'Metric', width: 300 },
+        { field: 'value', headerName: 'Value', width: 150 }
       ];
       
+    
     let rows = [];
-
+    
+    // push formatted data into rows
     for (let [key, value] of Object.entries(companyFinancials.metric)) {
         rows.push({
             id: key,
-            metric: key,
             value: value
         });
     }
 
-    console.log('ROWS: ', rows)
-
     return (
-        <div style={{ height: 400, width: '100%' }}>
-        <DataGrid 
-            rows={rows}
-            columns={columns}
-            // pageSize={5}
-            // checkboxSelection 
+        <div className="companyFinancialsList" style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
             />
         </div>
     );
