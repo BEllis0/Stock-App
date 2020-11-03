96669faa0808150c0bf5b6454e0a9038b9f83d7d
Brandon Ellis <brandonellis.email@gmail.com>
2020-11-02 21:53:57 -0700

    IPO Calendar view functionality finished

--- backend/routes/ipo_cal.js
+++ backend/routes/ipo_cal.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const ipoController = require('../controllers/ipo-controller.js');
 
-router.get('/ipo-calendar/:from/:to', ipoController.ipo_calendar);
+router.get('/ipo-calendar', ipoController.ipo_calendar);
 
 // export
 module.exports = router;
