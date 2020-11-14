5e198d65178a8285525942970eb1d229c789ebdd
Brandon Ellis <brandonellis.email@gmail.com>
2020-11-13 19:32:37 -0700

    moving code into components

--- src/components/sidebar.component.jsx
+++ src/components/sidebar.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Toolbar, Divider } from '@material-ui/core';
-import AddCircleIcon from '@material-ui/icons/AddCircleOutline';
 import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
 
 import StockSearchList from './Lists/StockSearchList.jsx';
@@ -13,41 +12,26 @@ const Sidebar = (props) => {
         height: window.screen.height
     };
 
-    if (props.stockName !== "" && props.stockName.length > 1 && props.searchItems !== undefined) {
-        return (
-            <div className="sidebar" style={sidebarStyles}>
-                <StockSearchList
-                    searchItems={props.searchItems}
-                    onSearchSelect={props.onSearchSelect}
-                    loggedIn={props.loggedIn}
-                    onAddWatchlist={props.onSearchSelect}
-                />
-            </div>
-        )
-    }
-
-    else {
-
-
-        return (
-            <div className="sidebar" style={sidebarStyles}>
+    return (
+        <div className="sidebar" style={sidebarStyles}>
             <Toolbar disableGutters={true} className="sidebarNewsButton">
                 <Link to="/" onClick={(props.displayMenu && props.onDisplayMenu )} className="nav-link"><h3>Stock News</h3></Link>
                 <Link to="/stock-search" className="nav-link"><h3>Stock Search</h3></Link>
                 <Divider variant="middle" />
                 <Link to="/ipo-calendar" className="nav-link"><h3>IPO Calendar</h3></Link>
+                <Link to="/watchlist" className="nav-link"><h3>Watchlist</h3></Link>
             </Toolbar>
-    
+
             <Divider variant="middle" />
-    
+
             <Toolbar className="addWatchlist">
                 <Divider orientation="vertical" />
                 <h4>Watchlist</h4>
                 
             </Toolbar>
-    
+
             <Divider variant="middle" />
-    
+
             {props.watchlistDb.length > 0 &&
             <div className="watchlistSection">
             {props.watchlistDb.map(watchlist => {
@@ -57,7 +41,7 @@ const Sidebar = (props) => {
                         <Link 
                             className="watchlistItemName"
                             to="/stocks"
-                            onClick={() => props.onSearchSelect(watchlist, watchlist)}>
+                            onClick={() => props.onStockSearchSelect(watchlist, watchlist)}>
                             {watchlist}
                         </Link>
                         <RemoveCircleOutlineIcon onClick={() => props.removeStock(watchlist)}  /> 
@@ -69,9 +53,8 @@ const Sidebar = (props) => {
             </div>
             }
             
-            </div>
-        );
-    }
+        </div>
+    );
     
 };
 
