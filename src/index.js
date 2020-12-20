2b00a4d8e1212e22b6a064987b3a346d83a049e9
Brandon Ellis <brandonellis.email@gmail.com>
2020-12-20 11:39:31 -0700

    renamed web sockets and added env vars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dotenv from 'dotenv';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// access to environmental vars
+dotenv.config();
 
 // ==========================
 // SET TOOLS TO WINDOW OBJECT
 // ==========================
 
 // realtime trade websocket
-window.socket = new WebSocket('wss://ws.finnhub.io?token=btl8tu748v6omckuoct0');
+window.currentPriceSocket = new WebSocket(`wss://ws.finnhub.io?token=${process.env.FINNHUB_API_KEY}`);
+window.watchlistSocket = new WebSocket(`wss://ws.finnhub.io?token=${process.env.FINNHUB_API_KEY}`);
 
 // set environment for server references
 window.environment = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
