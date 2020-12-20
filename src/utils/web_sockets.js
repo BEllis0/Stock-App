aafc3b0406012387918e1e56f1c3507d3a1b542d
Brandon Ellis <brandonellis.email@gmail.com>
2020-12-20 11:39:59 -0700

    fixed remove websocket function

--- src/utils/web_sockets.js
+++ src/utils/web_sockets.js
@@ -1,16 +1,28 @@
 
+let currentWebSocket;
+
+let watchlistSockets;
+
 // =====================
 // realtime trade socket
 // =====================
 
 export function setCurrentPriceWebSocket(stockName) {
     console.log(`Tracking current price for ${stockName}`);
-    window.socket.send(JSON.stringify({'type':'subscribe', 'symbol': stockName}));
+    // set current web socket, for removing later
+    currentWebSocket = stockName;
+    // create web socket
+    window.currentPriceSocket.send(JSON.stringify({'type':'subscribe', 'symbol': stockName}));
 };
 
 export function removePriceWebSocket(stockName) {
-    console.log(`Tracking current price for ${stockName}`);
-    window.socket.send(JSON.stringify({'type':'subscribe', 'symbol': stockName}));
+    console.log(`Removing price tracking for ${stockName}`);
+    
+    // if there is currently a socket in place
+    if (currentWebSocket) {
+        //  remove
+        window.currentPriceSocket.send(JSON.stringify({'type':'unsubscribe', 'symbol': stockName}));
+    }
 };
 
 export default {
