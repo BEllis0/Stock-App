571f630f95ac3c7d55d46c8b88465778d14048c6
Brandon Ellis <brandonellis.email@gmail.com>
2020-12-22 00:40:54 -0500

    updated handlers for updating realtime  web socket

--- src/utils/web_sockets.js
+++ src/utils/web_sockets.js
@@ -8,21 +8,24 @@ let watchlistSockets;
 // =====================
 
 export function setCurrentPriceWebSocket(stockName) {
-    console.log(`Tracking current price for ${stockName}`);
-    // set current web socket, for removing later
-    currentWebSocket = stockName;
-    // create web socket
-    window.currentPriceSocket.send(JSON.stringify({'type':'subscribe', 'symbol': stockName}));
+    // if current websocket is the one being asked for, do nothing
+    if (currentWebSocket && currentWebSocket === stockName) {
+        return;
+    } else {
+        // remove the current web socket
+        removePriceWebSocket(currentWebSocket);
+        // set the new web socket
+        currentWebSocket = stockName;
+        console.log(`Tracking current price for ${stockName}`);
+        // create web socket
+        window.currentPriceSocket.send(JSON.stringify({'type':'subscribe', 'symbol': stockName}));
+    }
+    
 };
 
 export function removePriceWebSocket(stockName) {
     console.log(`Removing price tracking for ${stockName}`);
-    
-    // if there is currently a socket in place
-    if (currentWebSocket) {
-        //  remove
-        window.currentPriceSocket.send(JSON.stringify({'type':'unsubscribe', 'symbol': stockName}));
-    }
+    window.currentPriceSocket.send(JSON.stringify({'type':'unsubscribe', 'symbol': stockName}));
 };
 
 export default {
